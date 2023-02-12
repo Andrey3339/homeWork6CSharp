c7cd2d557fd52953bd6acd7f0a3ceda313669599
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-02-12 19:37:35 +0300

    add task41

---
+++ task41/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+int count = 0;
+Console.Write("Введите с клавиатуры М чисел: ");
+int[] numbers = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+for(int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+        count++;
+}
+Console.WriteLine("Количество чисел больше нуля: {0}", count);
+
