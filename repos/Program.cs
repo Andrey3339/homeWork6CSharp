2de1a3b91f5365a3d1e0f313e2c91ee5e6e81069
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-02-16 17:41:01 +0300

    add repos

---
+++ repos/Program.cs
@@ -0,0 +1,31 @@
+﻿int K = 0;
+void RepositionSymbol(string s, int p1, int p2)
+{
+    if (p1 == p2)
+    {
+        K++;
+        Console.WriteLine(s + " " + K);
+    }
+    else
+    {
+        for (int i = p1; i <= p2; i++)
+        {
+            s = Swap(s, p1, i);
+            RepositionSymbol(s, p1 + 1, p2);
+        }
+    }
+}
+string Swap(string str, int i, int j)
+{
+    var charArray = str.ToCharArray();
+    char temp = charArray[i];
+    charArray[i] = charArray[j];
+    charArray[j] = temp;
+    return new String(charArray);
+}
+
+Console.Clear();
+Console.WriteLine("Введите последовательность символов от 1 до 8");
+string s = Console.ReadLine().ToUpper();
+RepositionSymbol(s, 0, s.Length - 1);
+
